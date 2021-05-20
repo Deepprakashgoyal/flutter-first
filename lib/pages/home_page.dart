aa49492a7a49439bfac92435a09e2962e7a6b348
deepprakashgoyal <deepprakash7868@gmail.com>
2021-05-20 18:45:16 +0530

    chap 5 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
-import "package:flutter/material.dart";
+import 'dart:convert';
 
-import '../change_name_card.dart';
+import "package:flutter/material.dart";
 import '../drawer.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,6 +12,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Chage This Text";
+  var url = Uri.https(
+    "jsonplaceholder.typicode.com",
+    "/photos",
+  );
+  var data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +41,20 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.grey[200],
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Card(
-            child:
-                ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text("ID: ${data[index]["id"]}"),
+                    leading: Image.network(data[index]["thumbnailUrl"]),
+                  ),
+                );
+              })
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
       // ignore: missing_required_param
